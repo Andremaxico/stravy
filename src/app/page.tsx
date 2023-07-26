5e31311f8d9ff33f37402b58b8ce1f57dcacdbfc
Andriy <andremaxico321@gmail.com>
2023-07-26 17:01:03 +0300

    added Ui Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+
 import { getAllUsers } from '@/lib/users/getAllUsers'
+import { getRGBColor, getAccessibleColor } from '@/utils';
 import Image from 'next/image'
+import { SearchRecipe } from './components/SearchRecipe';
 
 export default async function Home() {
   const data = await getAllUsers();
 
   console.log('users', data);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <SearchRecipe />
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,10 +17,42 @@ module.exports = {
   ],
   theme: {
     extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      textColor: {
+        skin: {
+          primary: withOpacity("--color-primary"),
+          a11y: withOpacity("--color-a11y"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          primary: withOpacity("--color-primary"),
+          a11y: withOpacity("--color-a11y"),
+        },
+      },
+      ringColor: {
+        skin: {
+          primary: withOpacity("--color-primary"),
+        },
+      },
+      borderColor: {
+        skin: {
+          primary: withOpacity("--color-primary"),
+          a11y: withOpacity("--color-a11y"),
+        },
       },
     },
   },
